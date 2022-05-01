e528c664010366b30f67e6bcb96e50ab8ac6fe8c
guvanch-amangeldiyev-idf <guvanch.amangeldiyev@idfinance.com>
2022-05-01 23:54:52 +0300

    dis

--- bin/design_patterns.dart
+++ bin/design_patterns.dart
@@ -1,8 +1,6 @@
 //import 'package:design_patterns/patterns/decorator/program/program.dart';
 
-import 'package:design_patterns/patterns/fly_weight/progrsm_fly/fly_weight.dart';
-
 void main(List<String> arguments) {
-  ProgramRunner dev = ProgramRunner();
-  dev.mainFly();
+  // ProgramRunner dev = ProgramRunner();
+  // dev.mainFly();
 }
